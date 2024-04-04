90568fffc75e40c3fb5d13f90a1db17a16c13787
clairekuo <claire.kuo@cloras.io>
2024-04-05 04:06:05 +0800

    feat: Implement input validation for API

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
 
 	"rate-exchange-api/internal/currencyexchange"
 )
@@ -17,6 +18,17 @@ func NewExchangeHandler(svc currencyexchange.CurrencyExchangeService) http.Handl
 		source := r.URL.Query().Get("source")
 		target := r.URL.Query().Get("target")
 		amount := r.URL.Query().Get("amount")
+
+		if source == "" || target == "" {
+			http.Error(w, "Source and target currencies must be specified", http.StatusBadRequest)
+			return
+		}
+
+		matched, err := regexp.MatchString(`^(\d+|\d{1,3}(,\d{3})*)(\.\d+)?$`, amount)
+		if err != nil || !matched {
+			http.Error(w, "Invalid amount format", http.StatusBadRequest)
+			return
+		}
 
 		convertedAmount, err := svc.ConvertCurrency(source, target, amount)
 		if err != nil {
